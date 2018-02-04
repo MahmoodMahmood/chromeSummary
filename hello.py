11d3b13024da036df1720cb9816735411365eaad
Mahmood Mahmood <hamoodzstyle@gmail.com>
2018-02-04 01:53:17 -0500

    plz work

--- hello.py
+++ hello.py
@@ -12,7 +12,7 @@ def data():
     #get requested url
     url = request.args.get('url')
     print(url)
-    resp = Flask.Response(url)
+    resp = Flask.make_response(url) #here you could use make_response(render_template(...)) too
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
     # return url
