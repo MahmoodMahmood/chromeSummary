ab3f09ad31e82613c4d606f3685836e9af5f6cdf
Mahmood Mahmood <hamoodzstyle@gmail.com>
2018-02-04 05:31:41 -0500

    trying to get ml algorithm into backend

--- Dank.py
+++ Dank.py
@@ -13,7 +13,7 @@ import numpy as np
 from watson_developer_cloud import NaturalLanguageUnderstandingV1
 from watson_developer_cloud.natural_language_understanding_v1 \
   import Features, EntitiesOptions, KeywordsOptions
-  
+
 def stemmedPunctuationTokenized(sentence,stopword=set(corpus.stopwords.words("english"))):
     s = stem.PorterStemmer()
     tokenizer = tk.RegexpTokenizer(r'\w+')
