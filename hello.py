fc0c06072da7f78badcdfe296c1ac55eae054c7b
Mahmood Mahmood <hamoodzstyle@gmail.com>
2018-02-03 23:27:31 -0500

    First Hello World!

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
+
+@app.route('/data')
+def data():
+    #get requested url
+    #url = request.args.get('url')
+    return "url is: "
